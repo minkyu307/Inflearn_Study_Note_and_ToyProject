c5ea89c6f49b5f9455cf9a144f4614086449156b
minkyu307 <82827785+minkyu307@users.noreply.github.com>
2021-07-27 15:58:28 +0900

    21.07.27
    간단한 도메인과 레파지토리, 그리고 그에 대한 테스트를 만드는것을 배움.

---
+++ basicBackEnd-spring/src/main/java/basicBackEnd/basicBackEndspring/BasicBackEndSpringApplication.java
@@ -0,0 +1,13 @@
+package basicBackEnd.basicBackEndspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBackEndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBackEndSpringApplication.class, args);
+	}
+
+}
---
+++ basicBackEnd-spring/src/test/java/basicBackEnd/basicBackEndspring/BasicBackEndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package basicBackEnd.basicBackEndspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBackEndSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
